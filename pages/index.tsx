5fe19e6ea8b48575b5499e8f73601f7e329f6e8f
George Keaton Williamson <georgekwilliamson@gmail.com>
2021-12-28 10:17:43 -0500

    third commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Second build
+         Third build
         </h1>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
